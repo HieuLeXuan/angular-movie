d60c1f99befab35f7a813d64a00976533df14918
HieuLeXuan <hieulexuan629@gmail.com>
2024-09-01 15:12:36 +0700

    deploy v2

--- src/app/shared/directives/img-missing.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { ImgMissingDirective } from './img-missing.directive';
-
-describe('ImgMissingDirective', () => {
-  it('should create an instance', () => {
-    const directive = new ImgMissingDirective();
-    expect(directive).toBeTruthy();
-  });
-});
